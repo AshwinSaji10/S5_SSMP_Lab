0a186ef59baf18c66ad1b933674bec6252bdc38b
Ashwin Saji <118591685+AshwinSaji10@users.noreply.github.com>
2023-02-19 07:50:51 +0530

    Add files via upload

---
+++ absolute_loader.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	FILE *fob,*fop;
+	char line[200];
+	char start[50];
+	int start_hex;
+	fob=fopen("obj.txt","r");
+	fop=fopen("output.txt","w");
+	while(fscanf(fob,"%s",line)!=EOF)
+	{
+		if(line[0]=='T')
+		{
+			int i,j;
+			for(i=2,j=0;i<=7;i++,j++)
+				start[j]=line[i];
+			start[j]='\0';
+			start_hex=strtol(start,NULL,16);
+			i=12;
+			while(line[i]!='$')
+			{
+				if(line[i]!='^')
+				{
+					fprintf(fop,"00%X\t%c%c\n",start_hex,line[i],line[i+1]);
+					printf("00%X\t%c%c\n",start_hex,line[i],line[i+1]);
+					start_hex+=1;
+					i+=2;
+				}
+				else
+					i++;
+			}
+		}
+	}
+	fclose(fob);
+	fclose(fop);
+}
